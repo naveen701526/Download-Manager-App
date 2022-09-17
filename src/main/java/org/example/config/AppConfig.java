bc6db78c9ce7104bd8d79205081f11f377de0a4e
naveen701526 <neonew8984@gmail.com>
2022-09-18 00:35:26 +0530

    download manager initial commit

---
+++ src/main/java/org/example/config/AppConfig.java
@@ -0,0 +1,6 @@
+package org.example.config;
+
+public class AppConfig {
+    public static final String DOWNLOAD_PATH = "C:\\Users\\neone\\Documents\\Download Folder";
+//    other config information
+}
